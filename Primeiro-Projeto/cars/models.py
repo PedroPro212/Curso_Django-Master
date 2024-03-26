61a45fddbdd1364eff8abbac34b73e7c1e8bd4c2
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2024-03-25 21:54:01 -0300

    Página do administrador

--- Primeiro-Projeto/cars/models.py
+++ Primeiro-Projeto/cars/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):  # Função para mascarar o Brand object
+        return self.name
+
+class Car(models.Model):    # A classe Car é uma herança da classe Model
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')    # Brand = Marca
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.model
