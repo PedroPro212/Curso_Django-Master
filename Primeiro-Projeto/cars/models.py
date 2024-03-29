54705c6bd036581abc3b164298286ea3cd26622f
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2024-03-28 22:04:52 -0300

    Adição de imagens

--- Primeiro-Projeto/cars/models.py
+++ Primeiro-Projeto/cars/models.py
@@ -4,7 +4,7 @@ class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
 
-    def __str__(self):  # Função para mascarar o Brand object
+    def __str__(self):  # Função para mascarar o brand object
         return self.name
 
 class Car(models.Model):    # A classe Car é uma herança da classe Model
@@ -13,7 +13,9 @@ class Car(models.Model):    # A classe Car é uma herança da classe Model
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')    # Brand = Marca
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model
