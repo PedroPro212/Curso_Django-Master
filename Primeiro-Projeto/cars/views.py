219c7fb5a7084ba08ef2551c4176d95865d21765
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2024-03-29 14:45:13 -0300

    Conceito de View; HTML e Django

--- Primeiro-Projeto/cars/views.py
+++ Primeiro-Projeto/cars/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def cars_view(request):
+    return render(
+        request, 
+        'pages/cars.html', 
+        {'cars': {'model': 'Astra 2.0'} }
+    )
